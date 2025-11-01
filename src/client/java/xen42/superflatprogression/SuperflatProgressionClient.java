3e85dfdedc16dfef4f4aa347386402f9254f1e9d
xen-42 <22628069+xen-42@users.noreply.github.com>
2025-11-01 18:57:57 -0400

    Fabric template

---
+++ src/client/java/xen42/superflatprogression/SuperflatProgressionClient.java
@@ -0,0 +1,10 @@
+package xen42.superflatprogression;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperflatProgressionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/xen42/superflatprogression/SuperflatProgressionDataGenerator.java
@@ -0,0 +1,11 @@
+package xen42.superflatprogression;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SuperflatProgressionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
