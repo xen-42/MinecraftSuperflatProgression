d46f3bb504f07be0da306608e6f5a3edce402d77
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-10 03:19:12 -0400

    make mycelium an alternative trade to podzol

--- src/main/java/xen42/superflatprogression/SuperflatProgressionVillagers.java
+++ src/main/java/xen42/superflatprogression/SuperflatProgressionVillagers.java
@@ -13,6 +13,7 @@ import net.minecraft.village.TradeOffers;
 
 public class SuperflatProgressionVillagers {
     public static void initialize() {
+    	// Ordinary
 		TradeOfferHelper.registerWanderingTraderOffers(1, factories -> {
 			factories.add(new TradeOffers.SellItemFactory(Items.SNIFFER_EGG, 10, 1, 12, 1));
 			factories.add(new TradeOffers.SellItemFactory(Items.TADPOLE_BUCKET, 3, 1, 12, 1));
@@ -21,11 +22,16 @@ public class SuperflatProgressionVillagers {
 			factories.add(new TradeOffers.SellItemFactory(Items.PRISMARINE_BRICKS, 3, 1, 12, 1));
 			factories.add(new TradeOffers.SellItemFactory(Items.AMETHYST_SHARD, 3, 1, 12, 1));
 			factories.add(new TradeOffers.SellItemFactory(Items.COBWEB, 3, 1, 12, 1));
-				
+
 			if (FabricLoader.getInstance().isModLoaded("peaceful-items")) {
 				var effigyAltar = Registries.ITEM.get(Identifier.of("peaceful-items", "effigy_altar"));
 				factories.add(new TradeOffers.SellItemFactory(effigyAltar, 20, 1, 1, 1));
 			}
+		});
+
+		// Special
+		TradeOfferHelper.registerWanderingTraderOffers(2, factories -> {
+			factories.add(new TradeOffers.SellItemFactory(Items.MYCELIUM, 3, 3, 6, 1));
 		});
     }
 }
